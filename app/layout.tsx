049dcf153b1d45476cf60a7068e7faf2a14ae103
eugene4447 <jenyzukovskij99@gmail.com>
2026-07-05 17:26:01 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
+import './globals.css'; // Добавляем эту строку!
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ru">
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
